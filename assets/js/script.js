a88551ba98adaf45706b44ae16f41b76f681b134
kotaewing <kota.ewing@gmail.com>
2021-04-24 15:36:59 -0600

    search bar and button now work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,64 @@
-var searchTest = "https://api.openweathermap.org/data/2.5/weather?q=draper&appid=92e0836afaeb6b857056f92c8fdb93c7"
+var weatherData = "";
 
-function coordinateFinder() {
+$('#submitSearch').on('click', function(event) {
+    event.preventDefault();
+    var searchInputEl = $("#citySearch")
+    var searchInput = $(searchInputEl).val().trim()
+    
+    coordinateFinder(searchInput)
+   
+    $(searchInputEl).val('')
+    
+});
+
+function coordinateFinder(searchInput) {
+    var searchTest = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&appid=92e0836afaeb6b857056f92c8fdb93c7";
 
     fetch (searchTest)
     .then(function(response) {
-        return response.json()
+        if (response.ok) {
+            return response.json()
+        } else {
+            alert("Something went wrong")
+        }
     })
     .then(function(data) {
         var lat = data.coord.lat
         var lon = data.coord.lon
         
-        oneCall(lat, lon);
+        weatherCall(lat, lon);
+    })
+    .catch(function(error) {
+        alert(error)
     })
 }
 
-function oneCall(lat, lon) {
-    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude={part}&appid=92e0836afaeb6b857056f92c8fdb93c7")
+function weatherCall(lat, lon) {
+    oneCallUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=minutely,hourly&appid=92e0836afaeb6b857056f92c8fdb93c7"
+    fetch(oneCallUrl)
     .then(function(response) {
-        return response.json()
+        if (response.ok) {
+            return response.json()
+        } else {
+            alert("Something went wrong")
+        }
     })
     .then(function(data) {
         console.log(data)
+        weatherData = data;
+    })
+    .catch(function(error) {
+        alert(error)
     })
 }
+
+function displayCurrentInfo(info) {
+    $('h2')
+    .text(searchInput)
+    
+}
+
+
+
+
+
